85ebb0b97b1ac182ea3188a32bed077812350cac
lexyhandi <lexihandi@gmail.com>
2021-02-06 16:20:07 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+const product = "Socks";
+
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "This is a thing",
+      image: "./assets/images/socks_green.jpg",
+      url: "www.vuemastery.com",
+      inStock: true,
+    };
+  },
+});
